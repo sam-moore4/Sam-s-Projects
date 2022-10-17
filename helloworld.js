e3ba051398e13897adb47dda91a26872497332f1
sam-moore98 <sam98.moore@hotmail.com>
2022-10-17 16:43:26 +1100

    commit

---
+++ helloworld.js
@@ -0,0 +1,29 @@
+function ThisIsTheName(Signature){
+    Body
+    }
+    function SayHello(){
+    console.log('Hello')
+    }
+
+    function Sum(A,B){
+        return A+B
+    }
+   console.log(Sum(5,1))
+
+   function Minus(A,B){
+    return A-B
+   }
+   
+   console.log(Minus(5,1))
+
+   function Multiply(A,B){
+    return A*B
+   }
+   
+   console.log(Multiply(5,2))
+
+   function Divide(A,B){
+    return A/B
+   }
+   
+   console.log(Divide(20,10))
