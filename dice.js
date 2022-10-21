fc58993176a627c235c5b6b56e8de00738ca2c54
sam-moore98 <sam98.moore@hotmail.com>
2022-10-22 10:52:03 +1100

    commit

--- dice.js
+++ dice.js
@@ -1,11 +1,13 @@
 // Function to generate random number 
+var x = diceSix();
+document.getElementById("answer").innerHTML = ('Dice Display is ' + " " + x)
+
 function diceSix(min, max) { 
     min = Math.ceil(1);
     max = Math.floor(6);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 } 
   
-console.log('Dice Display is') 
-  
-// Function call
-console.log(diceSix()); 
+function displayAnswer() {
+    document.getElementById("answer").style.display = "block"
+}
