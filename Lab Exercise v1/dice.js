b3be1d6a581669406f607c0c7725577098337e04
sam-moore98 <sam98.moore@hotmail.com>
2022-10-22 12:18:25 +1100

    commit

--- Lab Exercise v1/dice.js
+++ Lab Exercise v1/dice.js
@@ -5,6 +5,18 @@ function rollDice() {
     max = Math.floor(selectedValue);
     let x = Math.floor(Math.random() * (max - min + 1)) + min;
     document.getElementById("total").innerHTML = ('You rolled a ' + " " + x)
-    } 
+
+    if (x = 1){
+        let image = document.getElementById("dicepic");
+        image.src = "/Lab Exercise v1/img/dice-01.png";
+    }
+
+    if (x = 2){
+    let image = document.getElementById("dicepic");
+    image.src = "/Lab Exercise v1/img/dice-02.png";
+    }
+
+
+} 
   
 
