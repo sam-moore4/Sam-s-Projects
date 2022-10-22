fa12303a331b039b607c88c6a06e971422aabdd7
sam-moore98 <sam98.moore@hotmail.com>
2022-10-22 12:04:05 +1100

    commit

--- Lab Exercise v1/dice.js
+++ Lab Exercise v1/dice.js
@@ -1,8 +1,10 @@
 // Function to generate random number 
-function diceSix(min, max) { 
+function rollDice() { 
+    let selectedValue = document.getElementById("sidedDice").value;
     min = Math.ceil(1);
-    max = Math.floor(6);
+    max = Math.floor(selectedValue);
     let x = Math.floor(Math.random() * (max - min + 1)) + min;
     document.getElementById("total").innerHTML = ('You rolled a ' + " " + x)
     } 
   
+
