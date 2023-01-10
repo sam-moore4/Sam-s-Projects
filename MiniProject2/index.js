18bd13447e93a225d0f3c0acc1eea70d104b12e3
sam-moore98 <sam98.moore@hotmail.com>
2023-01-10 20:07:07 +1100

    commit

--- MiniProject2/index.js
+++ MiniProject2/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const teamsRoute = require('./routes/teamsRoute')
+const postRoute = require('./routes/postRoute')
 const app = express()
 const port = 3000
 
 app.use('/', express.static('public'))
 
 app.use('/teams', teamsRoute)
+
+app.use('/posts', postRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
